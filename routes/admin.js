71ebfbda7a0679ca1863827b9a63f41e022859c7
SrrihithaVarikooti18 <161626493+SrrihithaVarikooti18@users.noreply.github.com>
2024-12-07 13:51:41 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const { addTrain, bookSeat } = require('../controllers/admin'); // Importing admin controller functions
-const apiKeyMiddleware = require('../middleware/apiKey');  // API Key Middleware to authenticate admin
+const { addTrain, bookSeat } = require('../controllers/admin'); 
+const apiKeyMiddleware = require('../middleware/apiKey');  
 
 const router = express.Router();
 
-// Admin route to add a train
-router.post('/train', apiKeyMiddleware, addTrain); // Add train to the database
+router.post('/train', apiKeyMiddleware, addTrain); 
 
-// Admin route to book a seat on a train and update seat availability
-router.post('/train/book', apiKeyMiddleware, bookSeat); // Book seat and update available seats
+router.post('/train/book', apiKeyMiddleware, bookSeat); 
 
 module.exports = router;
