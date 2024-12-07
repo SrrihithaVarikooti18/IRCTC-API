6208dd84d5d8213e73f7379b2633c0409c565e20
SrrihithaVarikooti18 <161626493+SrrihithaVarikooti18@users.noreply.github.com>
2024-12-07 13:46:32 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,15 @@
-require('dotenv').config(); // Load environment variables
+require('dotenv').config(); 
 const { Sequelize } = require('sequelize');
 
 // Initialize Sequelize
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql', // Use 'postgres', 'sqlite', etc., if necessary
-    port: process.env.DB_PORT, // Ensure this matches your database configuration
+    dialect: 'mysql', 
+    port: process.env.DB_PORT, 
 });
 
-// Test the database connection
 sequelize.authenticate()
     .then(() => console.log('Database connected successfully!'))
     .catch((err) => console.error('Error connecting to the database:', err));
 
-module.exports = sequelize; // Export the initialized Sequelize instance
+module.exports = sequelize; 
